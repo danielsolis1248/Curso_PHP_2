92c19957ffab22e5ea049e89fb77c2c19fa3c0a9
danielsolis1248 <itic18.daniel.solis@iunis.edu.mx>
2023-10-23 14:28:35 -0600

    Ejercicios de while y do while

--- ejercicios.php
+++ ejercicios.php
@@ -1,7 +1,17 @@
 <?php
 
-$a = 7.5;
+$base = 2;
+$exp = 5;
 
+$resultado = 1;
+$i = 1;
+
+while ($i <= $exp) {
+    $resultado = $resultado * $base;
+    $i++;
+}
+
+echo "El valor de $base elevado a la $exp es: $resultado";
 // echo match($a) {
 //     1 => 'Domingo',
 //     2 => 'Lunes',
@@ -14,45 +24,38 @@ $a = 7.5;
 //     default => 'El valor no puede exceder a 7'
 // };
 
-switch ($a) {
-    case '1':
-        echo 'Domingo';
-        break;
+// switch ($a) {
+//     case '1':
+//         echo 'Domingo';
+//         break;
 
-    case $a >= 7.0 && $a <= 7.9:
-        echo 'Aprobaste';
-        break;
+//     case $a >= 7.0 && $a <= 7.9:
+//         echo 'Aprobaste';
+//         break;
 
-    case '3':
-        echo 'Martes';
-        break;
+//     case '3':
+//         echo 'Martes';
+//         break;
 
-    case '4':
-        echo 'Miercoles';
-        break;
+//     case '4':
+//         echo 'Miercoles';
+//         break;
 
-    case '5':
-        echo 'Jueves';
-        break;
+//     case '5':
+//         echo 'Jueves';
+//         break;
 
-    case '6':
-        echo 'Viernes';
-        break;
+//     case '6':
+//         echo 'Viernes';
+//         break;
 
-    case '7':
-        echo 'Sabado';
-        break;
+//     case '7':
+//         echo 'Sabado';
+//         break;
 
-    default:
-        echo 'El valor no puede exceder a mas de 7';
-        break;
-}
-
-/*$a = 8;
-$b = 8;
-
-$valor = $a > $b ? 'A es mayor que B' : ($a < $b ? 'B es mayor que B' : 'A y B son iguales');
-
-echo $valor;*/
+//     default:
+//         echo 'El valor no puede exceder a mas de 7';
+//         break;
+// }
 
 ?>
